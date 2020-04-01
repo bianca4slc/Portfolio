10f7de187bce7bd42704198a40b19ba0d5f8438a
3 Bianc$ <56326348+bianca4slc@users.noreply.github.com>
2020-04-01 00:07:17 -0600

    background image, navbar and links

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   var elems = document.querySelectorAll(".carousel");
   var instances = M.Carousel.init(elems, {
-    indicators: true,
+    indicators: false,
     numVisible: 5
   });
 });
